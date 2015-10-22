9e3d7dfe92ff7087a72502cb955c74a95bc5dfaa
Philippe Gauthier <philippe@philippe.io>
2015-10-22 11:55:55 -0700

    responsive update

--- src/js/components/slickInitialize.slick.js
+++ src/js/components/slickInitialize.slick.js
@@ -8,18 +8,10 @@ $('.carousel_tile_1').slick({
   autoplay: true,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 768,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 1,
+        slidesToScroll: 1,
       }
     }
   ]
