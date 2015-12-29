7539b76c6acf16f270cea26e1f7a4eafba1213c1
Philippe Gauthier <philippe@philippe.io>
2015-12-29 08:17:19 -0800

    removed resizing on property images

--- statamic/_add-ons/uuid/ft.uuid.php
+++ statamic/_add-ons/uuid/ft.uuid.php
@@ -3,7 +3,7 @@ require_once 'vendor/autoload.php';
 use Webpatser\Uuid\Uuid;
 class Fieldtype_uuid extends Fieldtype
 {
-    public function render_field()
+  public function render_field()
     {
     	$uuid = $this->field_data ?: Uuid::generate(4);
 			return '<input type="hidden" name="'.$this->fieldname.'" value="'.$uuid.'"/>';
