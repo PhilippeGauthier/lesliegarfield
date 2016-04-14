d2caf9aaa19c35bc530f86805b11259d8609e222
Philippe Gauthier <philippe@philippe.io>
2016-04-13 18:41:34 -0700

    logic test

--- src/js/components/Listing.react.js
+++ src/js/components/Listing.react.js
@@ -42,7 +42,7 @@ var Listing = React.createClass({
     if (listing.streeteasy_status == 'off-market') {
       listing.streeteasy_status = "";
     }
-    if (listing.openhouse == "true" ) {
+    if (listing.openhouse !== "" ) {
       listing.bottom_info_red = 'Open House';
       listing.bottom_info = "";
     }
